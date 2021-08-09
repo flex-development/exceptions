488097d6ad83639c2805477668f1d22791c24c5a
Lex <lex@flexdevelopment.llc>
2021-08-09 12:18:37 -0400

    chore(release): publish v2.0.1

--- .versionrc.js
+++ .versionrc.js
@@ -4,30 +4,33 @@
  */
 
 const sections = {
+  build: ':hammer: Build',
+  chore: ':pencil2: Housekeeping',
+  chore: ':truck: Continuous Integration & Deployment',
   docs: ':book: Documentation',
-  feat: ':package: Features',
+  feat: ':sparkles: Features',
   fix: ':bug: Fixes',
-  other: ':page_facing_up: Other Changes',
   perf: ':zap: Performance Updates',
+  refactor: ':recycle: Code Improvements',
   revert: ':rewind: Revert',
-  test: ':robot: Testing',
-  wip: ':construction: Work in Progress'
+  style: ':nail_care: Formatting & Structure',
+  test: ':robot: Testing'
 }
 
 module.exports = {
   types: [
     { type: 'feat', section: sections.feat },
     { type: 'fix', section: sections.fix },
-    { type: 'perf', section: sections.perf },
     { type: 'revert', section: sections.revert },
     { type: 'test', section: sections.test },
     { type: 'docs', section: sections.docs },
-    { type: 'wip', section: sections.wip },
-    { type: 'build', hidden: true },
-    { type: 'chore', hidden: true },
-    { type: 'ci', hidden: true },
-    { type: 'refactor', section: true },
-    { type: 'style', hidden: true }
+    { type: 'build', section: sections.build },
+    { type: 'refactor', section: sections.refactor },
+    { type: 'perf', section: sections.perf },
+    { type: 'style', section: sections.style },
+    { type: 'ci', section: sections.ci },
+    { type: 'chore', section: sections.chore },
+    { type: 'wip', hidden: true }
   ],
   releaseCommitMessageFormat: 'chore(release): publish v{{currentTag}}'
 }
